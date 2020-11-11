32cdcf8a57ccc41db8936b58fa10dd75ed51499e
Filipe Cancellier da Costa <filipecancelliercosta@gmail.com>
2020-11-11 22:25:28 +0000

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     oi
+      <div>
+        <label>Hours</label>
+        <input type="text" name="hours" />
+        <label>Minutes</label>
+        <input type="text" name="minutes" />
+        <label>Seconds</label>
+        <input type="text" name="seconds" />
+      </div>
+      <div>
+        Screen
+     </div>
+      <div>
+        <button>start</button>
+        <button>pause</button>
+      </div>
     </div>
   );
 }
