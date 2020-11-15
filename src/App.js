21bab673773935699d2d4168cab095c9b274d29c
Filipe Cancellier da Costa <filipecancelliercosta@gmail.com>
2020-11-15 12:43:53 +0000

    css interface created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [hourInput, setHourInput] = useState('00')
+  const [minuteInput, setMinuteInput] = useState('00')
+  const [secondInput, setSecondInput] = useState('00')
+  const [isHidden, setIsHidden] = useState(true)
+
+  function handleStart() {
+    setIsHidden(false)
+  }
+
+  function handleRestart() {
+    setIsHidden(true)
+  }
+  
   return (
-    <div className="App">
-      <div>
-        <label>Hours</label>
-        <input type="text" name="hours" />
-        <label>Minutes</label>
-        <input type="text" name="minutes" />
-        <label>Seconds</label>
-        <input type="text" name="seconds" />
-      </div>
-      <div>
-        Screen
-     </div>
-      <div>
-        <button>start</button>
-        <button>pause</button>
+    <div id="app">
+      {/* {isHidden && (
+
+      )} */}
+              <div className="inputContainer">
+          <label>Hours</label>
+          <input type="text" name="hours" onChange={(event) => { setHourInput(event.target.value) }} />
+          <label>Minutes</label>
+          <input type="text" name="minutes" onChange={(event) => { setMinuteInput(event.target.value) }} />
+          <label>Seconds</label>
+          <input type="text" name="seconds" onChange={(event) => { setSecondInput(event.target.value) }} />
+        </div>
+      {/* {!isHidden && (
+
+      )} */}
+              <div className="screenContainer">
+          <span>{`${hourInput}:${minuteInput}:${secondInput}`}</span>
+        </div>
+
+      <div className="btnContainer">
+        <button onClick={handleStart}>Start</button>
+        <button>Pause</button>
+        <button onClick={handleRestart}>Restart</button>
       </div>
     </div>
   );
