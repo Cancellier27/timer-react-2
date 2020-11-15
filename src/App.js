0be29ac226d4c93607ca8b173c86e7eb0553bd2a
Filipe Cancellier da Costa <filipecancelliercosta@gmail.com>
2020-11-15 13:18:29 +0000

    refactoring

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
 
   function handleStart() {
     setIsHidden(false)
+    clockLogic()
   }
 
   function handleRestart() {
@@ -21,28 +22,44 @@ function App() {
   }
 
   function clockLogic() {
-
+    let SecondInterval;
+    if (hourInput === 0 && minuteInput === 0) {
+      SecondInterval = setInterval(() => setSecondInput(secondInput => secondInput - 1 ) ,1000)
+    }
   }
-  
+
   return (
     <div id="app">
       {/* {isHidden && (
 
       )} */}
-              <div className="inputContainer">
-          <label>Hours</label>
-          <input type="text" name="hours" onChange={(event) => { setHourInput(event.target.value) }} />
-          <label>Minutes</label>
-          <input type="text" name="minutes" onChange={(event) => { setMinuteInput(event.target.value) }} />
-          <label>Seconds</label>
-          <input type="text" name="seconds" onChange={(event) => { setSecondInput(event.target.value) }} />
-        </div>
+      <div className="inputContainer">
+        <label>Hours</label>
+        <input
+          type="text"
+          name="hours"
+          onChange={(event) => { setHourInput(event.target.value) }} />
+        <label>Minutes</label>
+        <input
+          type="text"
+          name="minutes"
+          onChange={(event) => { setMinuteInput(event.target.value) }} />
+        <label>Seconds</label>
+        <input
+          type="text"
+          name="seconds"
+          onChange={(event) => { setSecondInput(event.target.value) }} />
+      </div>
       {/* {!isHidden && (
 
       )} */}
-              <div className="screenContainer">
-          <span>{`${showTimeWithZeros(hourInput)}:${showTimeWithZeros(minuteInput)}:${showTimeWithZeros(secondInput)}`}</span>
-        </div>
+      <div className="screenContainer">
+        <span>{
+        `${showTimeWithZeros(hourInput)}:
+        ${showTimeWithZeros(minuteInput)}:
+        ${showTimeWithZeros(secondInput)}`
+        }</span>
+      </div>
 
       <div className="btnContainer">
         <button onClick={handleStart}>Start</button>
